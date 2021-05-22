e1fcf12b7d875eafe0dcdb83948eb9a30cd8afa4
T'ienchieh Tai <81424789+Elfenreigen@users.noreply.github.com>
2021-05-22 11:34:25 +0800

    Add files via upload

---
+++ 工程问题建模与仿真/课程作业/作业二/2021-05-09 Initial Report/Clock Synchronization Problem/initialize.m
@@ -0,0 +1,10 @@
+mu_A = 37000;
+mu_B = 480000;
+n_samples = 1e5;
+n_max = 20;
+Life = zeros(1, n_samples);
+max_life = 9e4;
+mean_life = zeros(1, 20);
+reliability = zeros(1, 20);
+combo = [0, 3, 1; 1, 5, 1; 2, 3, 4; 4, 4, 4];
+FB_proportion = zeros(1, 20);
